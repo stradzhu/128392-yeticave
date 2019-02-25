78a3f754f9b3134cabe03f14c1d92cb8f35ca0eb
Дмитрий Страджу <stradzhu.d@wezom.com.ua>
2019-02-25 21:34:11 +0200

    Фикс предыдущего задания

--- schema.sql
+++ schema.sql
@@ -1,45 +1,55 @@
 -- Только для тестирования, чтобы каждый раз базу вручную не удалять
--- DROP DATABASE IF EXISTS yeticave;
+-- DROP DATABASE IF EXISTS yeticave_128392;
 
-CREATE DATABASE yeticave
+CREATE DATABASE yeticave_128392
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-USE yeticave;
+USE yeticave_128392;
 
 CREATE TABLE categories (
-  id    INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name  VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE bets (
-  id        INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  date_add  DATETIME DEFAULT CURRENT_TIMESTAMP,
-  price     INT(11) UNSIGNED NOT NULL,
-  user_id   INT(11) NOT NULL,
-  lot_id    INT(11) NOT NULL
+  id    INT UNSIGNED AUTO_INCREMENT,
+  name  VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE users (
-  id          INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  date_add    DATETIME DEFAULT CURRENT_TIMESTAMP,
-  email       VARCHAR(255) NOT NULL UNIQUE,
+  id          INT UNSIGNED AUTO_INCREMENT,
+  date_add    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  email       VARCHAR(255) NOT NULL,
   name        VARCHAR(255) NOT NULL,
   password    VARCHAR(255) NOT NULL,
-  image_path  VARCHAR(255),
-  contact     VARCHAR(255)
+  image_path  VARCHAR(255) NOT NULL,
+  contact     VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY (email)
 );
 
 CREATE TABLE lots (
-  id              INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  date_add        DATETIME DEFAULT CURRENT_TIMESTAMP,
+  id              INT UNSIGNED AUTO_INCREMENT,
+  date_add        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   title           VARCHAR(255) NOT NULL,
-  description     VARCHAR(1000),
-  image_path      VARCHAR(255),
-  price           INT(11) NOT NULL,
-  date_end        DATETIME DEFAULT CURRENT_TIMESTAMP,
-  bet_step        INT(11) NOT NULL,
-  category_id     INT(11) NOT NULL,
-  user_id_author  INT(11) NOT NULL,
-  user_id_winner  INT(11) NOT NULL
+  description     VARCHAR(1000) NOT NULL,
+  image_path      VARCHAR(255) NOT NULL,
+  price           INT UNSIGNED NOT NULL,
+  date_end        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  bet_step        INT UNSIGNED NOT NULL,
+  category_id     INT UNSIGNED NOT NULL,
+  user_id_author  INT UNSIGNED NOT NULL,
+  user_id_winner  INT UNSIGNED NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id_author) REFERENCES users (id),
+  FOREIGN KEY (user_id_winner) REFERENCES users (id),
+  FOREIGN KEY (category_id) REFERENCES categories (id)
+);
+
+CREATE TABLE bets (
+  id        INT UNSIGNED AUTO_INCREMENT,
+  date_add  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  price     INT UNSIGNED NOT NULL,
+  user_id   INT UNSIGNED NOT NULL,
+  lot_id    INT UNSIGNED NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (lot_id) REFERENCES lots (id)
 );
